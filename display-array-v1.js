67f646676303e93c03ae2cda8d838482ab5429a7
Jezzatator <jeremie.patot@protonmail.ch>
2023-06-08 15:21:25 +0200

    display-array-v1 fini

---
+++ display-array-v1.js
@@ -0,0 +1,8 @@
+let ukHun = ["Bing", "bang", "bong", "Sing", "sang", "song", "Ding", "dang", "dong"]
+
+const displayArray = tab => { 
+    str = tab.join(" - ")
+return str
+ }
+
+console.log(displayArray(ukHun))
